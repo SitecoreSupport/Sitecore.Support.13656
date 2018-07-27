3a5db622256c813c189917dce7cd71f64b58b315
DK\msb <msb@sitecore.net>
2018-07-27 16:23:09 -0400

    SXA CloudIndexFieldStorageValueFormatter is inherited now from CloudIndexFieldStorageValueFormatter

---
+++ src/Sitecore.Support.13656/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13656")]
+[assembly: AssemblyProduct("Sitecore.Support.13656")]
+[assembly: ComVisible(false)]
